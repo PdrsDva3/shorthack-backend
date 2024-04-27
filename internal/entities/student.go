4eb87fa0fb495471cbf4091ef6de40f0bc64be69
Lol <denis.kalinkin.05@gmail.com>
2024-04-27 12:02:57 +0300

    add student

--- internal/entities/student.go
+++ internal/entities/student.go
@@ -1,1 +1,18 @@
 package entities
+
+type StudentBase struct {
+	login string `json:"login"`
+	name  string `json:"name"`
+	level string `json:"level"`
+}
+
+type CreateStudent struct {
+	StudentBase
+	hashed_password string `json:"hashed_password"`
+}
+
+type Student struct {
+	StudentBase
+	mentors   []int `json:"mentors"`
+	StudentId int   `json:"id"`
+}
