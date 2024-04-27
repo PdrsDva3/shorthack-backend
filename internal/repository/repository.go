23e0a48995cadda928d8ef75341345e93c0698d5
Sve4ka <sve4ka_one@mail.ru>
2024-04-27 12:13:18 +0300

    repository db

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,20 @@
 package repository
+
+import (
+	"context"
+	"shorthack_backend/internal/entities"
+)
+
+//type MentorRepository interface {
+//	Create(ctx context.Context, user)
+//
+//}
+
+type StudentRepository interface {
+	Create(ctx context.Context, student entities.CreateStudent) (int, error)
+	Get(ctx context.Context, studentID int) (*entities.Student, error)
+	GetPassword(ctx context.Context, login string) (int, string, error)
+	UpdatePassword(ctx context.Context, studentID int, newPassword string) error
+	Delete(ctx context.Context, studentID int) error
+	AddMentor(ctx context.Context, studentID int, mentor entities.Mentor) (int, error)
+}
