0d6f58ee61e7a9c7e744a8e8bbc2a194676f408a
Lol <denis.kalinkin.05@gmail.com>
2024-04-27 11:52:55 +0300

    add mentor entity

--- internal/entities/mentor.go
+++ internal/entities/mentor.go
@@ -1,1 +1,19 @@
 package entities
+
+type MentorBase struct {
+	name  string `json:"name"`
+	login string `json:"login"`
+}
+
+type MentorCreate struct {
+	MentorBase
+	hashed_password string `json:"hashed_password"`
+}
+
+type Mentor struct {
+	MentorBase
+	MentorID  int      `json:"id"`
+	StudentID []int    `json:"student_id"`
+	level     string   `json:"level"`
+	knowledge []string `json:"knowledge"`
+}
